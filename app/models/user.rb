e20412da2d562ffde9ba3cc72c3bcb41d7ca4c71
muhenge <mugunga5@gmail.com>
2020-06-04 20:06:11 +0200

    Playing with Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :username, length: { in: 4..12 }, presence: true, uniqueness: true
   validates :email, format: { with: /\A(\S+)@(.+)\.(\S+)\z/ }, presence: true, uniqueness: true
   validates :password, length: { in: 6..16 }, presence: true, uniqueness: true
